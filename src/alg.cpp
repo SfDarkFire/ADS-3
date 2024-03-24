83fb7e2962284b90b51484424bae67cf05e795c3
SfDarkFire <160343903+SfDarkFire@users.noreply.github.com>
2024-03-24 22:56:28 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,27 @@
-// Copyright 2021 NNTU-CS
-
-int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+int cbinsearch(int* arr, int size, int value) {
+    int l = 0, r = size - 1, count = 0;
+    while (l <= r) {
+        int mid = r / 2;
+        if (arr[mid] == value) {
+            count++;
+            int tp = mid - 1;
+            while (tp >= 0 and arr[tp] == value) {
+                count++;
+                tp--;
+            }
+            tp = mid + 1;
+            while (tp < size and arr[tp] == value) {
+                count++;
+                tp++;
+            }
+            break;
+        }
+        else if (arr[mid] < value) {
+            l = mid + 1;
+        }
+        else {
+            r = mid - 1;
+        }
+    }
+    return count;
 }
